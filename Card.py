5a891828026f5957fc53bd557f58c5f164bfeda9
unfrikimas-jmog <josemoropezag27@gmail.com>
2025-10-06 22:31:03 -0300

    Creacion de clase carta y añadir funciones a field

---
+++ Card.py
@@ -0,0 +1,10 @@
+class Card:
+    def __init__(self, cardName: str, manaCost: int, manaPip: str, power: int, thougnes: int, cardType: str, cardSubtype: str):
+        self.cardName = cardName
+        self.manaCost = manaCost
+        self.manaPip = manaPip
+        self.power = power
+        self.thougnes = thougnes
+        self.cardType = cardType
+        self.cardSubtype = cardSubtype
+        pass
---
+++ Field.py
@@ -0,0 +1,4 @@
+class Field:
+    def __init__(self, playerID: int):
+        self.cards: list[str] = []
+        self.playerID = playerID
